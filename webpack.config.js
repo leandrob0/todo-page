21fc1967c916af7f4245b367539417dbfec84ce8
leandrob0 <leandro.bovino@gmail.com>
2021-12-01 18:49:24 -0300

    little changes in config

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
-    print: './src/page-load.js',
   },
   devtool: 'inline-source-map',
   devServer: {
